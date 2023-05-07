8a2d5e16c70f3058440fd784c3de1a4285fe1b57
shindeshubham10 <shindeshubham1100@gmail.com>
2023-05-07 22:27:11 +0530

    created auth app

--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
+	auth "GO/routes/Auth"
 	"fmt"
-	"net/http"
-
-	EmailVerification "GO/routes/Auth/EmailVerification"
-	ForgotPass "GO/routes/Auth/ForgotPass"
-	GoogleAuth "GO/routes/Auth/GoogleAuthenticator"
-	Signin "GO/routes/Auth/Signin"
-	Signup "GO/routes/Auth/Signup"
 )
 
 func main() {
-	fmt.Println("Starting Auth service")
-	http.HandleFunc("/signup", Signup.HandleSignup)
-	http.HandleFunc("/signin", Signin.HandleSignin)
-	http.HandleFunc("/emailVerify", EmailVerification.HandleEmailVerification)
-	http.HandleFunc("/forgotPass", ForgotPass.HandleForgotPass)
-	http.HandleFunc("/codeAuth", ForgotPass.HandleCodeAuth)
-	http.HandleFunc("/newPass", ForgotPass.HandleNewPassword)
-	http.HandleFunc("/googleAuth", GoogleAuth.HandleGoogleAuthenticate)
-	http.HandleFunc("/googleAuthCode", GoogleAuth.HandleCodeAuth)
-
-	fmt.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	authApp, err := auth.NewAuthApp()
+	if err != nil {
+		fmt.Println("Unable to start auth service")
+	}
+	authApp.Run()
 
 }
